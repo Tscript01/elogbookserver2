0954a66abf2b086ca702d64d801154d623e6e80f
Olaniyan Miracle <miracleolaniyan@yahoo.com>
2026-08-03 18:11:22 +0100

    new cors update

--- src/index.ts
+++ src/index.ts
@@ -11,12 +11,12 @@ const app: Express = express();
 const PORT = process.env.PORT ?? 5000;
 
 app.use(
-  cors({
-    origin: "*",
+cors({
+    origin: "http://localhost:3000",
     credentials: true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization", "Accept"]
-  })
+    allowedHeaders: ["Content-Type", "Authorization"]
+})
 );
 
 app.use(express.json());
